1d7169a36c8b73064e5ce9d5c52115e6153d04fb
jieuning <balllling92@gmail.com>
2025-05-25 17:41:07 +0900

    스타일링 학습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import style from "./index.module.css";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>h2</h2>
     </>
   );
 }
